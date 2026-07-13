abba6d3068ff915f4c3c43bc79d5e2417532cd8d
moharashid <rashidmohamud9865@gmail.com>
2026-07-13 16:13:04 +0300

    Add health router, project structure, and gitignore

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
-
+from app.api.health import router as health_router
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+app.include_router(health_router)
+
